f7ca200dbec8d5fc990d9c061f7dd4f949fc7512
Nicolás Gebauer <negebauer@uc.cl>
2016-07-13 04:35:37 -0400

    Create UCSTestCredentials.swift

---
+++ UCSiding/UCSTestCredentials.swift
@@ -0,0 +1,12 @@
+public struct UCSTestCredentials {
+
+    /// A valid username for the SIDING without `@uc.cl`
+    public func username() -> String {
+        return "myUsername"
+    }
+
+    /// The password for the `username()` user
+    public func password() -> String {
+        return "myPassword"
+    }
+}
