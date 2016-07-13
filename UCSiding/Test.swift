23797744efe8f360f44569a9cbd2ceae37199bb3
negebauer <negebauer@uc.cl>
2016-07-13 04:22:37 -0400

    Removing files

--- UCSiding/Test.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Test.swift
-//  Pods
-//
-//  Created by Nicolás Gebauer on 13-07-16.
-//
-//
-
-import Foundation
--- UCSiding/Tets.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Tets.swift
-//  Pods
-//
-//  Created by Nicolás Gebauer on 13-07-16.
-//
-//
-
-import Foundation
