dcc7342c6f01bde358b859d137de39428c9e077d
negebauer <negebauer@uc.cl>
2016-07-13 17:31:31 -0400

    Tests commented

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -20,30 +20,30 @@ class TableOfContentsSpec: QuickSpec {
 //                expect("time").toEventually( equal("done") )
 //            }
             
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        NSThread.sleepForTimeInterval(0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
+//            context("these will pass") {
+//
+//                it("can do maths") {
+//                    expect(23) == 23
+//                }
+//
+//                it("can read") {
+//                    expect("🐮") == "🐮"
+//                }
+//
+//                it("will eventually pass") {
+//                    var time = "passing"
+//
+//                    dispatch_async(dispatch_get_main_queue()) {
+//                        time = "done"
+//                    }
+//
+//                    waitUntil { done in
+//                        NSThread.sleepForTimeInterval(0.5)
+//                        expect(time) == "done"
+//
+//                        done()
+//                    }
+//                }
             }
         }
     }
