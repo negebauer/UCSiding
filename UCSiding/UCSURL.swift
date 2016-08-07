5e7fb4166ba2d5a24ecb05a8e2a5c5ef4a1d37d5
negebauer <negebauer@uc.cl>
2016-08-07 17:25:58 -0400

    URL constructor for a siding course

--- UCSiding/UCSURL.swift
+++ UCSiding/UCSURL.swift
@@ -6,7 +6,7 @@
 //
 //
 
-/// Container for siding's URLs
+/// Container for siding's static URLs
 public struct UCSURL {
     
     public static let domain = "intrawww.ing.puc.cl"
@@ -19,9 +19,65 @@ public struct UCSURL {
     public static let coursesPath = "/dirdes/ingcursos/cursos/index.phtml"
     public static let coursesURL = "\(main)\(coursesPath)"
     
+    public static let courseFilePath = coursesPath.stringByReplacingOccurrencesOfString("index.phtml", withString: "descarga.phtml")
+    public static let courseFileURL = "\(main)\(courseFilePath)"
+    
     public static let logoutPath =  "/logout.phtml"
     public static let logoutURL =  "\(main)\(logoutPath)"
     
     /// Append a course or file href to this url to have the course or file complete url
     internal static let courseMainURL = "\(main)/dirdes/ingcursos/cursos/"
+    
+    public struct CourseURL {
+        
+        let course: UCSCourse
+        var id: String { return course.idSiding }
+        
+        init(course: UCSCourse) {
+            self.course = course
+        }
+        
+        private func url(action: String) -> String {
+            return "\(coursesURL)?accion_curso=\(action)&id_curso_ic=\(id)"
+        }
+        
+        public func fileUrl(file: UCSFile) -> String {
+            if file.isFolder() {
+                return "\(coursesURL)?id_curso_ic=\(id)&accion_curso=carpetas&acc_carp=abrir_carpeta&id_carpeta=\(file.idSidingFolder!)"
+                
+            } else if file.isFile() {
+                return "\(courseFileURL)?id_curso_ic=\(id)&id_archivo=\(file.idSidingFile!)"
+            } else {
+                return ""
+            }
+        }
+
+        public func news() -> String {
+            return url("avisos")
+        }
+        
+        public func program() -> String {
+            return url("programa")
+        }
+        
+        public func calendar() -> String {
+            return url("calendario")
+        }
+        
+        public func forms() -> String {
+            return url("cuestionarios")
+        }
+        
+        public func students() -> String {
+            return url("alumnos")
+        }
+        
+        public func grades() -> String {
+            return url("notas")
+        }
+        
+        public func forum() -> String {
+            return url("foro")
+        }
+    }
 }
